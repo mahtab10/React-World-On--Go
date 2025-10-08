3145a91525169341b6bd643cc7a4d5c5a7a0ab19
mahtab10 <mm635243@gmail.com>
2025-10-08 18:42:39 +0600

    buttton added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
     console.log(country);
+
+    const handleVisited = () => {
+        console.log(`Visited ${country.name.common}`);
+    }
+
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name : {country.name.common} </h3>
             <h3>Capital : Capital of {country.name.common} is "{country.capital.capital[0]}"</h3>
-            <h3>Area : {country.area.area}</h3>
+            <h3>Area : {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</h3>
             <h3>Population : {country.population.population}</h3>
             {/* <h3>Currencies : {country}</h3> */}
+            <button onClick={handleVisited}>Not visited</button>
+
         </div>
     );
 };
